abc052ca87920a4aa4fee92282dcd9137a47c9a4
Uahh <hnyrarara@gmail.com>
2021-09-09 00:01:05 +0800

    v1.0

---
+++ slscq/slscq/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  slscq
+//
+//  Created by Uahh on 2021/9/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
